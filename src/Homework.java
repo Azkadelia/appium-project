3974c3d538512a938f3de30f69679845565e3ed6
Azkadelia <test@gmail.com>
2021-06-15 23:02:44 +0300

    homework_week_3.2

--- src/Homework.java
+++ src/Homework.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class Homework {
 
@@ -38,12 +41,87 @@ public class Homework {
         );
     }
 
-    @Test
-    public void searchFieldHasText() {
-        assertElementHasText(
-                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Search Wikipedia",
-                "Text is not displayed"
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
         );
     }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds) {
+
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    @Test
+    public void myFirstTest() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find search Wikipedia input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Animal",
+                "Cannot find search input",
+                5
+
+        );
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.withMessage("Cannot find 'Topic with Animal'\n");
+        List<WebElement> titleElements = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']")));
+        int animalsFound = 0;
+        for (WebElement el : titleElements) {
+            if (el.getText().toLowerCase().contains("animal")) {
+                animalsFound = animalsFound + 1;
+            }
+        }
+
+        Assert.assertTrue("Only " + animalsFound + " topics were found", animalsFound >= 2);
+
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X to cancel search",
+                5
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "Titles are still presented on the list",
+                10
+        );
+    }
+
 }
